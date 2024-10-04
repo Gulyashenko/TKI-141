635a0f7565b3cc2cceaab933840d2ef766fea0f8
Gulyashenko <99470525+Gulyashenko@users.noreply.github.com>
2024-10-04 21:37:41 +0300

    Update Task 1-1.c

--- Task 1-1/Task 1-1.c
+++ Task 1-1/Task 1-1.c
@@ -2,47 +2,44 @@
 #include <stdio.h>
 
 /**
-* @brief функция для вычисления значения а.
-* @param x Значение переменной x.
-* @param y Значение переменной y.
-* @param z Значение переменной z.
-* @return Результат вычислени а.
-*/
-double func_a(double x, double y, double z);
-
-/**
 * @brief функция для вычисления значения b.
 * @param x Значение переменной x.
 * @param y Значение переменной y.
 * @param z Значение переменной z.
-* @return Результат вычислени b.
+* @return Результат вычисления b.
 */
-double func_b(double x, double y, double z);
+double func_b(const double x, const double y, const double z);
+
+/**
+* @brief функция для вычисления значения а.
+* @param x Значение переменной x.
+* @param y Значение переменной y.
+* @param z Значение переменной z.
+* @return Результат вычисления а.
+*/
+double func_a(const double x, const double y, const double z)
 
 /**
  * @brief Точка входа в программу.
  * @return Возвращает в случае успеха.
 */
-int main()
+int main(void)
 {
 	const double x = 0.2;
 	const double y = 0.004;
 	const double z = 1.1;
 
-	double a = func_a(x, y, z);
-	double b = func_b(x, y, z);
-
-	printf("a = %f \nb = %f", a, b);
+	printf("a = %f \nb = %f", func_a(x, y, z), func_b(x, y, z));
 
 	return 0;
 }
 
-double func_a(double x, double y, double z)
+double func_a(const double x, const double y, const double z)
 {
 	return pow(sin(pow((x * x + z), 2)), 3) - sqrt(x / y);
 }
 
-double func_b(double x, double y, double z)
+double func_b(const double x, const double y, const double z)
 {
 	return x * x / z + cos(pow((x + y), 3));
 }
