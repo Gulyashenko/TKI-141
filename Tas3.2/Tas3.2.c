9d0dad0dc981abd42bfa86513f9b48d0092a4249
Gulyashenko <igor89037078337@yandex.ru>
2024-11-18 12:14:06 +0300

    изменения в коде

--- Tas3.2/Tas3.2.c
+++ Tas3.2/Tas3.2.c
@@ -1,6 +1,7 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
 #include <float.h>
+#include <math.h>
 
 /**
 * @brief вычисляет сумму первых n членов последовательности (k = 1, 2, 3 ..., n).
@@ -11,11 +12,10 @@ double func_for_finding_summ_1(const int n);
 
 /**
 * @brief вычисляет сумму всех членов последовательности, не меньших заданного числа e
-* @param n значение последнего члена последовательности
 * @param e значение параметра e
 * @return возвращает сумму всех членов последовательности, не меньших заданного числа e
 */
-double func_for_finding_summ_2(const int n,const double e);
+double func_for_finding_summ_2(const double e);
 
 /**
 * @brief вычисляет рекуррентное соотношение относительно данного значения элемента последовательности
@@ -41,36 +41,41 @@ int main(void) {
 	printf("введите значение e:\t");
 	double e = input();
 
-	printf("%lf\n%lf", func_for_finding_summ_1(n), func_for_finding_summ_2(n, e));
+	printf("%lf\n%lf", func_for_finding_summ_1(n), func_for_finding_summ_2(e));
 
 	return 0;
 }
 
 double func_for_finding_summ_1(const int n) {
-	double current = 1.0;
+	double current = 1;
 	double summ = current; 
 
 	for (int k = 0; k < n; k++) {
 		current *= recurrent(k);
 		summ += current;
+		//printf("%lf", &summ);
 	}
+	printf("конец первой функции");
 	return summ;
 }
 
-double func_for_finding_summ_2(const int n,const double e) {
+double func_for_finding_summ_2(const double e) {
 
-	double current = 1.0;
+	double current = 1;
 	double summ = current;
 
-	for (int k = 0; current<e+DBL_EPSILON <= n; k++) {
+	for (int k = 0; abs(current)>e-DBL_EPSILON; k++) {
 		current *= recurrent(k);
 		summ += current;
+		
+		//printf("%lf", &current);
 	}
+	printf("конец второй функции");
 	 return summ;
 }
 
 double recurrent(const int k) {
-	return 1 / (double)(4 * k * k + 6 * k + 2);
+	return pow(-1, k) / (double)(4 * k * k + 6 * k + 2);
 }
 
 double input(void) {
